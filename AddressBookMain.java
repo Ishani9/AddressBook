b641690a8c29bd8c547dda583b09bb5243a3d0be
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-09-29 18:07:35 +0530

    UC 2: Adding a new Contact to Address Book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,41 @@
-public class AddressBookMain {
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program..");
-	}
+import java.util.Scanner;
 
+public class AddressBookMain {
+	public static void main(String[] args) {
+		//variables
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		String state;
+		int zip;
+		long  phoneNum;
+		String email;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to Address Book");
+		System.out.println("Enter following details : ");
+		
+		System.out.println("First Name : ");
+		firstName = sc.nextLine();
+		System.out.println("Last Name : ");
+		lastName = sc.nextLine();
+		System.out.println("Address : ");
+		address = sc.nextLine();
+		System.out.println("City : ");
+		city = sc.nextLine();
+		System.out.println("State : ");
+		state = sc.nextLine();
+		System.out.println("ZIP : ");
+		zip = sc.nextInt();
+		System.out.println("Phone number : ");
+		phoneNum = sc.nextLong();
+		sc.nextLine();
+		System.out.println("Email ID : ");
+		email = sc.nextLine();
+		
+		Person person1 = new Person(firstName, lastName, address, city, state, zip, phoneNum, email);		
+	}
 }
+
