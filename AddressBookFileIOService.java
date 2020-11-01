4bc01078d5bd62aa91aaf65a8302e00a6f63596f
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-01 20:24:08 +0530

    Added: Ability to Read/Write Contacts as CSV File

--- AddressBookFileIOService.java
+++ AddressBookFileIOService.java
@@ -3,8 +3,16 @@ package assignment;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class AddressBookFileIOService {
 	
@@ -35,4 +43,58 @@ public class AddressBookFileIOService {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * UC 14
+	 * 
+	 */
+	public void writeDataCSV(Map<String, AddressBook> stateAddressBookMap) {
+
+		File file = new File(
+				"C:\\Users\\Ishani\\eclipse-workspace\\gradleAssignment\\Addressbook.csv");
+
+		try {
+			FileWriter outputfile = new FileWriter(file);
+			CSVWriter writer = new CSVWriter(outputfile);
+			List<String[]> data = new ArrayList<String[]>();
+			String[] header = { "First Name", "Last Name", "Address", "City", "State", "ZIP", "Phone Number",
+					"Email ID" };
+			data.add(header);
+			stateAddressBookMap.values().stream().map(entry -> entry.getPersonList())
+					.forEach(entry2 -> entry2.forEach(person -> {
+						String[] personData = { person.getFirstName(), person.getLastName(), person.getAddress(),
+								person.getCity(), person.getState(), Integer.toString(person.getZip()),
+								Long.toString(person.getPhoneNumber()), person.getEmail() };
+						data.add(personData);
+					}));
+
+			writer.writeAll(data);
+			writer.close();
+			System.out.println("Data entered successfully to Addressbook.csv file.");
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readDataCSV() {
+		try {
+			FileReader filereader = new FileReader(
+					"C:\\Users\\Ishani\\eclipse-workspace\\gradleAssignment\\Addressbook.csv");
+			CSVReader csvReader = new CSVReader(filereader);
+			String[] nextRecord;
+
+			System.out.println("Reading from CSV file line by line...");
+			while ((nextRecord = csvReader.readNext()) != null) {
+				for (String cell : nextRecord) {
+					System.out.print(cell + "\t  ");
+				}
+				System.out.println();
+			}
+			csvReader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
