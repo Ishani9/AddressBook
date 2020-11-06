d1fdf7dbb8c65bfc08e0e6bf13f3803e7b7dd01f
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-06 23:24:06 +0530

    Added: Ability to update the Contact Information and ensure the memory is in Sync with the DB

--- AddressBookFileServiceTest.java
+++ AddressBookFileServiceTest.java
@@ -11,10 +11,34 @@ import assignment.Person;
 
 public class AddressBookFileServiceTest {
 	
+	/**
+	 * UC 16
+	 * 
+	 * @throws DatabaseException
+	 */
 	@Test
 	public void givenContactDataInDB_WhenRetrieved_ShouldMatchContactCount() throws DatabaseException {
 		AddressBookFileIOService addressBookService = new AddressBookFileIOService();
 		List<Person> contactData = addressBookService.readContactData(IOService.DB_IO);
+		assertEquals(8, contactData.size());
+	}
+	
+	/**
+	 * UC 17
+	 * 
+	 * checking Updated info of a persons is in sync with database
+	 * 
+	 * @throws DatabaseException
+	 * @throws SQLException
+	 */
+	@Test
+	public void givenNewDataForContact_WhenUpdated_ShouldBeInSync() throws DatabaseException {
+		AddressBookFileIOService addressBookService = new AddressBookFileIOService();
+		List<Person> contactData = addressBookService.readContactData(IOService.DB_IO);
+		addressBookService.updatePersonsPhone("Bill Games", 123456789);
+		addressBookService.readContactData(IOService.DB_IO);
+		boolean result = addressBookService.checkContactDataSync("Bill Games");
+		assertEquals(true, result);
 		assertEquals(8, contactData.size());
 	}
 }
